ae0509a910c026139cf7082213ffbb1afae9cfb6
AKRAMUL HASAN <akramul@AKRAMULs-MacBook-Pro.local>
2022-10-20 07:28:45 +0600

    another changes

--- dom/move-element/script.js
+++ dom/move-element/script.js
@@ -3,9 +3,6 @@
 ** .woocommerce-product-details__short-description -  This is the class in which after the targeted div will added
 
 *** Learn more about jQuery insertAfter: https://api.jquery.com/insertAfter/#selector
-
-
-
 */
 
 var expire = document.querySelector(".wps_gw_expiry_notice");
