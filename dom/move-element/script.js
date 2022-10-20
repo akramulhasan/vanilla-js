8ae64ca9370d3e709453304fd33a5b3413956bbb
AKRAMUL HASAN <akramul@AKRAMULs-MacBook-Pro.local>
2022-10-20 07:20:52 +0600

    added insertAfter() with example

---
+++ dom/move-element/script.js
@@ -0,0 +1,13 @@
+/*
+** var expire is the targeted Div that will be moved
+** .woocommerce-product-details__short-description -  This is the class in which after the targeted div will added
+
+*** Learn more about jQuery insertAfter: https://api.jquery.com/insertAfter/#selector
+
+
+
+*/
+
+var expire = document.querySelector(".wps_gw_expiry_notice");
+
+$(expire).insertAfter(".woocommerce-product-details__short-description");
